0a4d0b33e6331088a3b291995fc2cdd873cecc20
IMPWNG <matisse.ach+github.com>
2022-06-02 15:05:04 -0400

    Basic project structure + routes

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./home.scss"
+
+export default function Home() {
+  return (
+    <div className='home'>
+    <h1 className='title'>Hello</h1>
+    </div>
+  )
+}
