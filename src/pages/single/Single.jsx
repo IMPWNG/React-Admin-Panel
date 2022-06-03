978b07b4c398a6cb8493dd55d1716f9fd4a5bccb
IMPWNG <matisse.ach+github.com>
2022-06-03 17:01:38 -0400

    userSingle style in progress

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+
 import "./single.scss"
 
 function Single() {
   return (
-    <div>Single</div>
-  )
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <div className="title">Information</div>
+            <div className="item">
+              <img
+                src="https://bobbyhadz.com/images/blog/react-usestate-dynamic-key/thumbnail.webp"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 555.555.555</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    123 Main St, New York, NY 12345
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">
+                    USA
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Single
